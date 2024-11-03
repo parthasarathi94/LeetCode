6e7de60a1a87399187a55041ccd34664d2a76d59
Partha Sarathi Phukon <129682011+parthasarathi94@users.noreply.github.com>
2024-11-03 10:23:21 +0530

    LeetCode 796

---
+++ 796. Rotate String.cpp
@@ -0,0 +1,17 @@
+// Questiion Link: https://leetcode.com/problems/rotate-string/description/
+
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        int n = s.length();
+        int m = goal.length();
+
+        if( n != m) return false;
+
+        if((s+s).find(goal) != string::npos){
+            return true;
+        }
+
+        return false;
+    }
+};
