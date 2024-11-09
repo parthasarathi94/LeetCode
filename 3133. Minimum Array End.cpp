7dd7bea4f67cc662dbbc59ffa5b2ccd9116a2522
Partha Sarathi Phukon <129682011+parthasarathi94@users.noreply.github.com>
2024-11-09 11:38:22 +0530

    LeetCode 3133

---
+++ 3133. Minimum Array End.cpp
@@ -0,0 +1,15 @@
+// Question Link: https://leetcode.com/problems/minimum-array-end/description/
+
+class Solution {
+public:
+    long long minEnd(int n, int x) {
+        
+        long long num = x;
+
+        for(int i=1; i<n; i++){
+            num = (num + 1) | x;
+        }
+
+        return num;
+    }
+};
