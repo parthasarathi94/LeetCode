67844a40cacf7776c1c70aaf5ad85f7e7bbc95d8
Partha Sarathi Phukon <129682011+parthasarathi94@users.noreply.github.com>
2025-01-05 14:29:58 +0530

    LeetCode 2381

---
+++ 2381. Shifting Letters II.cpp
@@ -0,0 +1,36 @@
+//Question Link: https://leetcode.com/problems/shifting-letters-ii/description/
+
+class Solution {
+public:
+    string shiftingLetters(string s, vector<vector<int>>& shifts) {
+        int n = s.length();
+        vector<int>diff(n, 0);
+        for(auto &shift : shifts){
+            int L = shift[0];
+            int R = shift[1];
+            int dir = shift[2];
+
+            int x;
+            if(dir == 1) x = 1;
+            else x = -1;
+
+            diff[L] += x;
+            if(R+1 < n){
+                diff[R+1] -= x; 
+            }    
+        }
+
+        for(int i=1; i<n; i++){
+            diff[i] += diff[i-1];
+        }
+
+        for(int i=0; i<n; i++){
+            int shift = diff[i]%26;
+            if(shift < 0) shift += 26;
+            
+            s[i] = (((s[i] - 'a') + shift) % 26) + 'a';
+        }
+
+        return s;
+    }
+};
