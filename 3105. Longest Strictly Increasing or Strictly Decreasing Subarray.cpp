469ef5259f7b3d25aa793da7e3f63b436b4c9f49
Partha Sarathi Phukon <129682011+parthasarathi94@users.noreply.github.com>
2025-02-03 18:30:41 +0530

    LeetCode 3105

---
+++ 3105. Longest Strictly Increasing or Strictly Decreasing Subarray.cpp
@@ -0,0 +1,28 @@
+//Question Link: https://leetcode.com/problems/longest-strictly-increasing-or-strictly-decreasing-subarray/description/
+
+class Solution {
+public:
+    int longestMonotonicSubarray(vector<int>& nums) {
+        int n = nums.size();
+
+        int result = 1;
+        int increasing = 1;
+        int decreasing = 1;
+        for(int i = 1; i < n; i++) {
+            if(nums[i] > nums[i-1]) {
+                increasing++;
+                decreasing = 1;
+                result = max(result, increasing);
+            } else if(nums[i] < nums[i-1]) {
+                decreasing++;
+                increasing = 1;
+                result = max(result, decreasing);
+            } else {
+                increasing = 1;
+                decreasing = 1;
+            }
+        }
+
+        return result;
+    }
+};
