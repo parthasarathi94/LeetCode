0f8c6180dd34bcbc379528632fb17c5e824c039e
Partha Sarathi Phukon <129682011+parthasarathi94@users.noreply.github.com>
2025-02-12 09:28:02 +0530

    LeetCode 2342

---
+++ 2342. Max Sum of a Pair With Equal Sum of Digits.cpp
@@ -0,0 +1,28 @@
+//Question Link: https://leetcode.com/problems/max-sum-of-a-pair-with-equal-sum-of-digits/
+
+class Solution {
+public:
+
+    int getDigitSum(int num){
+        int sum = 0;
+        while(num > 0){
+            sum += (num % 10);
+            num = num/10;
+        }
+        return sum;
+    }
+
+    int maximumSum(vector<int>& nums) {
+        int n = nums.size();
+        int result = -1;
+        unordered_map<int, int>mp;
+        for(int i=0; i<n; i++){
+            int digitSum = getDigitSum(nums[i]);
+            if(mp.count(digitSum)){
+                result = max(result, nums[i] + mp[digitSum]);
+            }
+            mp[digitSum] = max(mp[digitSum], nums[i]);
+        }
+        return result;
+    }
+};
