516fd46fe0061220aec13e494cc7de6dc1a563cb
Partha Sarathi Phukon <129682011+parthasarathi94@users.noreply.github.com>
2025-03-01 10:53:47 +0530

    LeetCode 2460

---
+++ 2460. Apply Operations to an Array.cpp
@@ -0,0 +1,22 @@
+//Question Link: https://leetcode.com/problems/apply-operations-to-an-array/description/
+
+class Solution {
+public:
+    vector<int> applyOperations(vector<int>& nums) {
+        int n = nums.size();
+        int j = 0;
+        for(int i = 0; i < n; i++) {
+            if(i+1 < n && nums[i] == nums[i+1] && nums[i] != 0) {
+                nums[i] *= 2;
+                nums[i+1] = 0;
+            }
+            if(nums[i] != 0) {
+                if(i != j) {
+                    swap(nums[i], nums[j]);
+                }
+                j++;
+            }
+        }
+        return nums;
+    }
+};
