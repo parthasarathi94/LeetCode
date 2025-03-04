daef948914f3518b767f37f2d9feec2e8e3be2de
Partha Sarathi Phukon <129682011+parthasarathi94@users.noreply.github.com>
2025-03-04 10:58:16 +0530

    LeetCode 1780

---
+++ 1780. Check if Number is a Sum of Powers of Three.cpp
@@ -0,0 +1,16 @@
+//Question Link: https://leetcode.com/problems/check-if-number-is-a-sum-of-powers-of-three/description/
+
+class Solution {
+public:
+    bool checkPowersOfThree(int n) {
+        while(n > 0) {
+            if(n%3 == 2) {
+                return false;
+            }
+
+            n = n/3;
+        }
+
+        return true;
+    }
+};
