52f75f296c69bf5e2f4fd8b045fe366983dc4a4b
Partha Sarathi Phukon <129682011+parthasarathi94@users.noreply.github.com>
2025-03-07 10:08:04 +0530

    LeetCode 2523

---
+++ 2523. Closest Prime Numbers in Range.cpp
@@ -0,0 +1,42 @@
+//Question Link: https://leetcode.com/problems/closest-prime-numbers-in-range/description/
+
+class Solution {
+public:
+
+    bool isPrime(int num) {
+        if(num == 1) {
+            return false;
+        }
+        for(int div = 2; div*div <= num; div++) {
+            if(num % div == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    vector<int> closestPrimes(int left, int right) {
+        vector<int> primes;
+
+        for(int num = left; num <= right; num++) {
+            if(isPrime(num) == true) {
+                if(!primes.empty() && num - primes.back() <= 2) {
+                    return {primes.back(), num};
+                }
+                primes.push_back(num);
+            }
+        }
+
+        int minDiff = INT_MAX;
+        vector<int> result = {-1, -1};
+        for(int i = 1; i < primes.size(); i++) {
+            int diff = primes[i] - primes[i-1];
+            if(diff < minDiff) {
+                minDiff = diff;
+                result = {primes[i-1], primes[i]};
+            }
+        }
+
+        return result;
+    }
+};
