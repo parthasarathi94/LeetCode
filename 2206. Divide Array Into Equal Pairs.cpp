2c8e994279f0b932e79ca9675e748aee5376ccf0
Partha Sarathi Phukon <129682011+parthasarathi94@users.noreply.github.com>
2025-03-17 13:29:22 +0530

    LeetCode 2206

---
+++ 2206. Divide Array Into Equal Pairs.cpp
@@ -0,0 +1,17 @@
+//Question Link: https://leetcode.com/problems/divide-array-into-equal-pairs/description/
+
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) {
+        unordered_map<int, int>freq;
+        for(int &num : nums){
+            freq[num]++;
+        }
+
+        for(auto &it : freq){
+            if(it.second % 2 != 0) return false;
+        }
+
+        return true;
+    }
+};
