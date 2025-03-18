2b48009e618eff932c32869a77a2023359836c64
Partha Sarathi Phukon <129682011+parthasarathi94@users.noreply.github.com>
2025-03-18 13:29:50 +0530

    LeetCode 2401

---
+++ 2401. Longest Nice Subarray.cpp
@@ -0,0 +1,19 @@
+//Question Link: https://leetcode.com/problems/longest-nice-subarray/description/
+
+class Solution {
+public:
+    int longestNiceSubarray(vector<int>& nums) {
+        int n = nums.size();
+        int result = 0;
+        for(int i=0; i<n; i++){
+            int mask = 0;
+            for(int j=i; j<n; j++){
+                if((mask & nums[j]) != 0) break;
+                result = max(result, j-i+1);
+                mask |= nums[j];
+            }
+        }
+
+        return result;
+    }
+};
