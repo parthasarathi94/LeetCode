3e51a9370c2f3d5e9d1a2b732743e58379e5b894
Partha Sarathi Phukon <129682011+parthasarathi94@users.noreply.github.com>
2025-03-30 09:59:59 +0530

    LeetCode 763

---
+++ 763. Partition Labels.cpp
@@ -0,0 +1,27 @@
+//Question Link: https://leetcode.com/problems/partition-labels/description/
+
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        int n = s.length();
+        vector<int> result;
+        vector<int> mp(26, -1);
+        for(int i = 0; i < n; i++) {
+            int idx = s[i] - 'a';
+            mp[idx] = i;
+        }
+
+        int i = 0;
+        int start = 0;
+        int end = 0;
+        while(i < n) {
+            end = max(end, mp[s[i]-'a']);
+            if(i == end) {
+                result.push_back(end-start+1);
+                start = end+1;
+            }
+            i++;
+        }
+        return result;
+    }
+};
