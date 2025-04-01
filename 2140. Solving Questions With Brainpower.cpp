095b0f8c56e3c22c7da9bd1cede4ac5418443043
Partha Sarathi Phukon <129682011+parthasarathi94@users.noreply.github.com>
2025-04-01 18:34:43 +0530

    LeetCode 2140

---
+++ 2140. Solving Questions With Brainpower.cpp
@@ -0,0 +1,19 @@
+//Question Link: https://leetcode.com/problems/solving-questions-with-brainpower/description/
+
+class Solution {
+public:
+    long long mostPoints(vector<vector<int>>& questions) {
+        int n = questions.size();
+        
+        if(n == 1)
+            return questions[0][0];
+        
+        vector<long long> t(200001);
+ 
+        for(int i = n-1; i >= 0; i--) {
+            t[i] = max(questions[i][0] + t[i + questions[i][1] + 1], t[i+1]);
+        }
+        
+        return t[0];
+    }
+};
