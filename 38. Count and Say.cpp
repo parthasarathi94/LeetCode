1d54644bd436b537659311a02d92414b6c67316f
Partha Sarathi Phukon <129682011+parthasarathi94@users.noreply.github.com>
2025-04-18 10:48:36 +0530

    LeetCode 38

---
+++ 38. Count and Say.cpp
@@ -0,0 +1,20 @@
+//Question Link: https://leetcode.com/problems/count-and-say/description/
+
+class Solution {
+public:
+    string countAndSay(int n) {
+        if(n == 1) return "1";
+        string result = "";
+        string say = countAndSay(n-1);
+        for(int i=0; i<say.length(); i++){
+            char ch = say[i];
+            int count = 1;
+            while(i<say.length()-1 && say[i] == say[i+1]){
+                count++;
+                i++;
+            }
+            result += to_string(count) + string(1,ch);
+        }
+        return result;
+    }
+};
